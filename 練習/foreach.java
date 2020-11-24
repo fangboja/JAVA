3bb909d1fa16e24ddafa7ca954ace00b8893eeeb
方柏傑 <62335065+fangboja@users.noreply.github.com>
2020-11-24 19:29:08 +0800

    Add files via upload

---
+++ 練習/foreach.java
@@ -0,0 +1,11 @@
+
+public class foreach {
+
+    public static void main(String[] args) {
+        String exam[]={"SCJP","SCWD","SCMAD"};
+        for(String ex:exam){
+            System.out.print(ex+",");
+        }
+    }
+    
+}
