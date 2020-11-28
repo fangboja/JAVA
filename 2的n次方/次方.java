50f5454384f46c5702f8d158d904b376bc7245e6
方柏傑 <62335065+fangboja@users.noreply.github.com>
2020-11-28 13:46:35 +0800

    Add files via upload

---
+++ 2的n次方/次方.java
@@ -0,0 +1,12 @@
+
+public class ¦¸¤è {
+
+	public static void main(String[] args) {
+		long a=1L;
+		System.out.println(a<<32);
+		System.out.println(a<<38);
+		System.out.println(a<<49);
+
+	}
+
+}
